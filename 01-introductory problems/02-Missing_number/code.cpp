b495a04206506e8d796462ddef2aa96c93a6ea7c
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-05 15:43:00 +0530

    add solution and approach of missing numbers problem

--- 01-introductory problems/02-Missing_number/code.cpp
+++ 01-introductory problems/02-Missing_number/code.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+#define long long int
 
 int main(int argc, char const *argv[])
 {
-    long long a;
-    cin >> a;
-    cout << a << " ";
-    while (a != 1)
+    int n;
+    cin >> n;
+    vector<int> visited(n);
+    for (int i = 0; i < n - 1; i++)
     {
-        if (a % 2 == 0)
+        int temp = 0;
+        cin >> temp;
+        visited[temp]++;
+    }
+    int temp = n;
+    for (int i = 1; i < visited.size(); i++)
+    {
+        if (visited[i] == 0)
         {
-            a /= 2;
-            cout << a << " ";
-        }
-        else
-        {
-            a = 3 * a + 1;
-            cout << a << " ";
+            temp = i;
+            break;
         }
     }
+    cout<< temp <<endl;
     return 0;
 }
